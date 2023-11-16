fffcd7601de5a82a4e91dfc6b8dedea27dfac7d7
JY LEE <'wndyd0110@gmail.com'>
2023-11-16 23:05:55 +0900

    반장 구하기

--- algorithm/src/array/TemporaryLeader_11.java
+++ algorithm/src/array/TemporaryLeader_11.java
@@ -5,59 +5,60 @@ import java.util.Scanner;
 
 public class TemporaryLeader_11 {
 
-	static int solution(int n, int[][] check) {
+	static int solution(int n, int[][] arr) {
 		
-		int[] count = new int[n+1];
 		int max = 0;
+		int result = 1;
 		
-		for(int i = 0; i < 5; i++) {
-			for(int j = 0; j< n; j++) {
-				for(int z = n-1-j; z > 0; z--) {
+		// 처음 for문의 i는 count할 학생 (행)
+		// 두번쨰 for문의 j는 count할 학생의 비교 대상 (행)
+		// 세번쨰 for문은 학년 (열)
+		for(int i = 1; i<=n; i++) {
+			
+			int cnt = 0;
+			
+			for(int j = 1; j<=n; j++) {
+				
+				for(int x = 1; x<=5; x++) {
 					
-					if(check[j][i] == check[j+z][i] && (count[j] <= i+1) && (count[j+z] <= i+1)) {
-						
-						count[j]++;
-						count[j+z]++;
-						
+					if(arr[i][x] == arr[j][x]) {
+						cnt++;
+						break;
 					}
 					
 				}
 			}
-		}
-		System.out.println(Arrays.toString(count));
-		
-		
-		for(int i = 0; i < count.length; i++) {
+			// max와 cnt가 같을 때 앞에 학생이 반장이므로 max가 cnt 미만
+			if(max < cnt) {
+				
+				max = cnt;
+				result = i;
+				
+			}
 			
-			max = count[i] > count[i+1] ? i : i+1;
-			
 		}
 		
-		return max;
+		return result;
+		
 	}
 	
+	
 	public static void main(String[] args) {
-		
-		
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		
-		int[][] check = new int[n][5];
+		int[][] arr = new int[n+1][6];
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j<5; j++) {
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j<=5; j++) {
 				
-				int m = sc.nextInt();
-				
-				check[i][j] = m;
-				
+				int x = sc.nextInt(); // 1 ~ 5학년 때 몇 반 인지에 대한 입력 값
+				arr[i][j] = x;
 			}
 		}
-		
-		System.out.println(TemporaryLeader_11.solution(n, check));
-
+		System.out.println(TemporaryLeader_11.solution(n, arr));
 	}
 
 }
